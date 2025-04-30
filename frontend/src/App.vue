3dc748510249c417ee1dd638ad631e04d93a9574
Vladimir DOMBROVSKI <vladimir.dombrovski@bso.co>
2025-04-30 14:18:14 +0200

    Redirect to login page if user is not authenticated

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,65 +1,70 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
-import { computed, getCurrentInstance, onMounted, ref } from "vue";
-import { authStore } from "./stores/auth";
-import { securityStore } from "./stores/security";
-import { settingsStore } from "@/stores/settings";
-import { Notifications } from "@kyvg/vue3-notification";
+  import { RouterLink, RouterView } from 'vue-router';
+  import { computed, getCurrentInstance, onMounted, ref } from "vue";
+  import { authStore } from "./stores/auth";
+  import { securityStore } from "./stores/security";
+  import { settingsStore } from "@/stores/settings";
+  import { Notifications } from "@kyvg/vue3-notification";
 
-const appGlobal = getCurrentInstance().appContext.config.globalProperties
-const auth = authStore()
-const sec = securityStore()
-const settings = settingsStore()
+  const appGlobal = getCurrentInstance().appContext.config.globalProperties
+  const auth = authStore()
+  const sec = securityStore()
+  const settings = settingsStore()
 
-onMounted(async () => {
-  console.log("Starting WireGuard Portal frontend...");
+  onMounted(async () => {
+    console.log("Starting WireGuard Portal frontend...");
 
-  await sec.LoadSecurityProperties();
-  await auth.LoadProviders();
+    await sec.LoadSecurityProperties();
+    await auth.LoadProviders();
 
-  let wasLoggedIn = auth.IsAuthenticated;
-  try {
-    await auth.LoadSession();
-    await settings.LoadSettings(); // only logs errors, does not throw
+    let wasLoggedIn = auth.IsAuthenticated;
+    try {
+      await auth.LoadSession();
+      await settings.LoadSettings(); // only logs errors, does not throw
 
-    console.log("WireGuard Portal session is valid");
-  } catch (e) {
-    if (wasLoggedIn) {
-      console.log("WireGuard Portal invalid - logging out");
-      await auth.Logout();
+      console.log("WireGuard Portal session is valid");
+    } catch (e) {
+      if (wasLoggedIn) {
+        console.log("WireGuard Portal invalid - logging out");
+        await auth.Logout();
+      }
+    }
+
+    if (!auth.IsAuthenticated && window.location.href != '/app/#/login') {
+      window.location.href = '/app/#/login';
+      return
+    }
+
+    console.log("WireGuard Portal ready!");
+  })
+
+  const switchLanguage = function (lang) {
+    if (appGlobal.$i18n.locale !== lang) {
+      localStorage.setItem('wgLang', lang);
+      appGlobal.$i18n.locale = lang;
     }
   }
 
-  console.log("WireGuard Portal ready!");
-})
+  const languageFlag = computed(() => {
+    // `this` points to the component instance
+    let lang = appGlobal.$i18n.locale.toLowerCase();
+    if (!appGlobal.$i18n.availableLocales.includes(lang)) {
+      lang = appGlobal.$i18n.fallbackLocale;
+    }
+    const langMap = {
+      en: "us",
+      pt: "pt",
+      uk: "ua",
+      zh: "cn",
+      ko: "kr",
 
-const switchLanguage = function (lang) {
-  if (appGlobal.$i18n.locale !== lang) {
-    localStorage.setItem('wgLang', lang);
-    appGlobal.$i18n.locale = lang;
-  }
-}
+    };
+    return "fi-" + (langMap[lang] || lang);
+  })
 
-const languageFlag = computed(() => {
-  // `this` points to the component instance
-  let lang = appGlobal.$i18n.locale.toLowerCase();
-  if (!appGlobal.$i18n.availableLocales.includes(lang)) {
-    lang = appGlobal.$i18n.fallbackLocale;
-  }
-  const langMap = {
-    en: "us",
-    pt: "pt",
-    uk: "ua",
-    zh: "cn",
-    ko: "kr",
-
-  };
-  return "fi-" + (langMap[lang] || lang);
-})
-
-const companyName = ref(WGPORTAL_SITE_COMPANY_NAME);
-const wgVersion = ref(WGPORTAL_VERSION);
-const currentYear = ref(new Date().getFullYear())
+  const companyName = ref(WGPORTAL_SITE_COMPANY_NAME);
+  const wgVersion = ref(WGPORTAL_VERSION);
+  const currentYear = ref(new Date().getFullYear())
 
 </script>
 
@@ -69,8 +74,7 @@ const currentYear = ref(new Date().getFullYear())
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
       <a class="navbar-brand" href="/"><img :alt="companyName" src="/img/header-logo.png" /></a>
-      <button aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"
-        data-bs-target="#navbarTop" data-bs-toggle="collapse" type="button">
+      <button aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-bs-target="#navbarTop" data-bs-toggle="collapse" type="button">
         <span class="navbar-toggler-icon"></span>
       </button>
 
@@ -89,8 +93,7 @@ const currentYear = ref(new Date().getFullYear())
 
         <div class="navbar-nav d-flex justify-content-end">
           <div v-if="auth.IsAuthenticated" class="nav-item dropdown">
-            <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
-              href="#" role="button">{{ auth.User.Firstname }} {{ auth.User.Lastname }}</a>
+            <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button">{{ auth.User.Firstname }} {{ auth.User.Lastname }}</a>
             <div class="dropdown-menu">
               <RouterLink :to="{ name: 'profile' }" class="dropdown-item"><i class="fas fa-user"></i> {{ $t('menu.profile') }}</RouterLink>
               <RouterLink :to="{ name: 'settings' }" class="dropdown-item" v-if="auth.IsAdmin || !settings.Setting('ApiAdminOnly')"><i class="fas fa-gears"></i> {{ $t('menu.settings') }}</RouterLink>
@@ -118,25 +121,25 @@ const currentYear = ref(new Date().getFullYear())
         <div class="col-6 text-end">
           <div :aria-label="$t('menu.lang')" class="btn-group" role="group">
             <div class="btn-group" role="group">
-              <button aria-expanded="false" aria-haspopup="true" class="btn btn btn-secondary pe-0"
-                data-bs-toggle="dropdown" type="button"><span :class="languageFlag" class="fi"></span></button>
+              <button aria-expanded="false" aria-haspopup="true" class="btn btn btn-secondary pe-0" data-bs-toggle="dropdown" type="button"><span :class="languageFlag" class="fi"></span></button>
               <div aria-labelledby="btnGroupDrop3" class="dropdown-menu" style="">
                 <a class="dropdown-item" href="#" @click.prevent="switchLanguage('de')"><span class="fi fi-de"></span> Deutsch</a>
                 <a class="dropdown-item" href="#" @click.prevent="switchLanguage('en')"><span class="fi fi-us"></span> English</a>
                 <a class="dropdown-item" href="#" @click.prevent="switchLanguage('fr')"><span class="fi fi-fr"></span> Français</a>
-  	          	<a class="dropdown-item" href="#" @click.prevent="switchLanguage('ko')"><span class="fi fi-kr"></span> 한국어</a>
+                <a class="dropdown-item" href="#" @click.prevent="switchLanguage('ko')"><span class="fi fi-kr"></span> 한국어</a>
                 <a class="dropdown-item" href="#" @click.prevent="switchLanguage('pt')"><span class="fi fi-pt"></span> Português</a>
                 <a class="dropdown-item" href="#" @click.prevent="switchLanguage('ru')"><span class="fi fi-ru"></span> Русский</a>
                 <a class="dropdown-item" href="#" @click.prevent="switchLanguage('uk')"><span class="fi fi-ua"></span> Українська</a>
                 <a class="dropdown-item" href="#" @click.prevent="switchLanguage('vi')"><span class="fi fi-vi"></span> Tiếng Việt</a>
                 <a class="dropdown-item" href="#" @click.prevent="switchLanguage('zh')"><span class="fi fi-cn"></span> 中文</a>
-                
+
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </footer></template>
+  </footer>
+</template>
 
 <style></style>
